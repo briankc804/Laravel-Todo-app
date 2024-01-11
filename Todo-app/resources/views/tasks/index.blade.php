2183ed17c1315b21697282460cb9cd5753050ebb
briankc804 <briankc804@gmail.com>
2024-01-11 09:48:37 +0300

    First initial commit

---
+++ Todo-app/resources/views/tasks/index.blade.php
@@ -0,0 +1,9 @@
+<html>
+    <head>
+        <title>Todo App</title>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+    </head>
+    <body>
+        <h1>Task list</h1>
+    </body>
+</html>
---
+++ Todo-app/routes/web.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+
+
+Route::get('/', function () {
+    return view('tasks.index');
+});
+
+
+Route::get('/tasks/create', function() {
+    return view('tasks.create');
+});
